2b335a3165f7cced0fc1a5fa6d175ec43e4e6a1d
OurenOosterveen <ouren.oosterveen@home.nl>
2016-02-19 10:49:16 +0100

    Loaded Getal rij and Made Non-Recursive

--- Non-Recursive tree traversal/src/TreeNode.java
+++ Non-Recursive tree traversal/src/TreeNode.java
@@ -5,6 +5,8 @@ class TreeNode<E> {
     protected E element;
     protected TreeNode<E> left;
     protected TreeNode<E> right;
+    protected boolean isPopped = false;
+    protected TreeNode<E> root;
     public TreeNode(E e) {
         element = e;
     }
--- Non-Recursive tree traversal/src/inOrderTreversal.java
+++ Non-Recursive tree traversal/src/inOrderTreversal.java
@@ -9,6 +9,7 @@ public class inOrderTreversal{
     {
         // Create the root node
         TreeNode<Integer> root = new TreeNode<>(60);
+        root.root = root;
 
         // Create the left child node
         root.left = new TreeNode<>(55);
@@ -22,8 +23,16 @@ public class inOrderTreversal{
         root.right.left = new TreeNode<>(90);
         root.right.right = new TreeNode<>(110);
 
-
+        System.out.println("inOrder: ");
+        // order should be: 30, 55, 56, 60, 90, 100, 110
         inOrder(root);
+        System.out.println("preOrder: ");
+        // order should be: 60, 55, 30, 56, 100, 90, 110
+        preOrder(root);
+        System.out.println("postOrder: ");
+        // order should be: 30, 56, 55, 90, 110, 100, 60
+        postOrder(root);
+
     }
 
     public static void inOrder(TreeNode tree)
@@ -47,8 +56,59 @@ public class inOrderTreversal{
         }
     }
 
-    public static void preOrder()
+    public static void preOrder(TreeNode tree)
     {
+        TreeNode current = tree;
+        Stack stack = new Stack();
 
+        while (current != null || !stack.isEmpty())
+        {
+            if (current != null)
+            {
+                stack.push(current);
+                System.out.println(current.element);
+                current = current.left;
+            }
+            else
+            {
+                current = (TreeNode)stack.pop();
+                current = current.right;
+            }
+        }
+    }
+
+    public static void postOrder(TreeNode tree)
+    {
+        TreeNode current = tree;
+        Stack stack = new Stack();
+
+        //while (current != null || !stack.isEmpty())
+        while (true)
+        {
+            if (current != null)
+            {
+                if (current.right != null)
+                {
+                    stack.push(current.right);
+                }
+                stack.push(current);
+                current = current.left;
+                continue;
+            }
+
+            if (stack.isEmpty())
+                return;
+            current = (TreeNode)stack.pop();
+
+            if (current.right != null && !stack.isEmpty()&& current.right == stack.peek())
+            {
+                stack.pop();
+                stack.push(current);
+                current = current.right;
+            } else {
+                System.out.println(current.element);
+                current = null;
+            }
+        }
     }
 }
